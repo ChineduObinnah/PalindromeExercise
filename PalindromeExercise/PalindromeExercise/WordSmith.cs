e811b4c1a15544ac775cc1db826d8df6611f6a1c
Chinedu Obinnah~ <cobinnah@yahoo.com>
2023-06-05 18:46:04 -0500

    Palindrome Complete

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //    var backwards = "";
+            //    for( int i =word.Length -1; i >= 0; i--)
+            //    {
+            //        backwards += word[i];   
+            //    }
+            var wordCharArr = word.ToCharArray();
+            Array.Reverse(wordCharArr);
+            var backwards = new string(wordCharArr);
+
+            if (word.ToLower() == backwards.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+
+}
